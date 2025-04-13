7dce93097e22f813cf9f81724a72b672579ac2bf
hippieperm <leess0133@gmail.com>
2025-04-14 01:12:41 +0900

    홈 페이지의 AppBar에 TextField의 최대 줄 수 및 입력 장식 추가, GestureDetector의 onTap 기능을 람다식에서 블록으로 변경

--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -1,15 +1,38 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  final TextEditingController textEditingController = TextEditingController();
+
+  HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () => FocusScope.of(context).unfocus(),
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
       child: Scaffold(
         appBar: AppBar(
-          title: const TextField(),
+          title: TextField(
+            maxLines: 1,
+            controller: textEditingController,
+            decoration: InputDecoration(
+                hintText: '검색어를 입력해 주세요.',
+                border: MaterialStateOutlineInputBorder.resolveWith(
+                  (states) {
+                    if (states.contains(WidgetState.focused)) {
+                      return OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(color: Colors.black),
+                      );
+                    }
+                    return OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(color: Colors.grey),
+                    );
+                  },
+                )),
+          ),
         ),
         body: const Text('HomePage'),
       ),
