3cd6c1226eab5b3a4a707a67a6c818db4830eff0
hippieperm <leess0133@gmail.com>
2025-04-14 00:50:46 +0900

    의존성 추가 및 홈 페이지로의 기본 경로 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_book_search_app/ui/home/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,6 +10,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return  MaterialApp(
+      home: HomePage(),
+    );
   }
 }
