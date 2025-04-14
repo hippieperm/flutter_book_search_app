869a5a14e268fd253001e5598abccb61b09504d2
hippieperm <leess0133@gmail.com>
2025-04-14 18:39:11 +0900

    책 검색 기능을 수정하여 비동기 방식으로 결과를 반환하도록 변경하고, API 응답을 JSON으로 파싱하여 Book 객체 리스트를 반환하도록 구현함.

--- lib/data/repository/book_repository.dart
+++ lib/data/repository/book_repository.dart
@@ -1,14 +1,26 @@
+import 'dart:convert';
+
+import 'package:flutter_book_search_app/data/model/book.dart';
 import 'package:http/http.dart';
 
 class BookRepository {
-  Future<void> searchBooks(String query) async {
+  Future<List<Book>> searchBooks(String query) async {
     final client = Client();
     final response = await client.get(
-      Uri.parse('https://openapi.naver.com/v1/search/book.json?query=$query'),
-      headers: {
-        'X-Naver-Client-Id': 'ffaahhmjjWUf13LIQiGo',
-        'X-Naver-Client-Secret': 'LPwo0ZpKlG',
-      },
-    );
+        Uri.parse('https://openapi.naver.com/v1/search/book.json?query=$query'),
+        headers: {
+          'X-Naver-Client-Id': 'ffaahhmjjWUf13LIQiGo',
+          'X-Naver-Client-Secret': 'LPwo0ZpKlG',
+        });
+    if (response.statusCode == 200) {
+      Map<String, dynamic> map = jsonDecode(response.body);
+      final items = List.from(map['items']);
+      final iterable = items.map((e) {
+        return Book.fromJson(e);
+      });
+
+      return iterable.toList();
+    }
+    return [];
   }
 }
