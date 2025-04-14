dabd468b2625d8f6f459cee13ffd4fd42a51e682
hippieperm <leess0133@gmail.com>
2025-04-14 15:33:53 +0900

    상세 페이지에서 InAppWebView를 사용하여 외부 웹사이트를 표시하도록 변경함.

--- lib/ui/detail/detail_page.dart
+++ lib/ui/detail/detail_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 class DetailPage extends StatelessWidget {
   const DetailPage({super.key});
@@ -9,7 +10,16 @@ class DetailPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('해리포터의 마법사의 '),
       ),
-      body: const Text('DetailPage'),
+      body: InAppWebView(
+        initialUrlRequest: URLRequest(
+          url: WebUri('http://www.naver.com'),
+        ),
+        initialSettings: InAppWebViewSettings(
+          userAgent:
+              'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+          javaScriptEnabled: true,
+        ),
+      ),
     );
   }
 }
