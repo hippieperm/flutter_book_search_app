0f3efa4700bdf4e3f670b053b9b584afbd6dc23d
hippieperm <leess0133@gmail.com>
2025-04-14 16:46:53 +0900

    책 검색 기능을 비동기 방식으로 변경하고, API 호출 시 필요한 헤더를 추가하여 외부 API와의 통신을 개선함.

--- lib/data/repository/book_repository.dart
+++ lib/data/repository/book_repository.dart
@@ -1,9 +1,14 @@
 import 'package:http/http.dart';
 
 class BookRepository {
-  void searchBooks(String query) {
+  Future<void> searchBooks(String query) async {
     final client = Client();
-    client
-        .get(Uri.parse('https://openapi.naver.com/v1/search/book.json?query=$query'));
+    final response = await client.get(
+      Uri.parse('https://openapi.naver.com/v1/search/book.json?query=$query'),
+      headers: {
+        'X-Naver-Client-Id': 'ffaahhmjjWUf13LIQiGo',
+        'X-Naver-Client-Secret': 'LPwo0ZpKlG',
+      },
+    );
   }
 }
